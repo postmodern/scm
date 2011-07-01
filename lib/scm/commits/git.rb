6cd7825acd0c6d2238e28e8edd454df6d907666a
Postmodern <postmodern.mod3@gmail.com>
2011-06-30 19:05:03 -0700

    Added files for Commits::Git and Commits::SVN.

---
+++ lib/scm/commits/git.rb
@@ -0,0 +1,9 @@
+require 'scm/commits/commit'
+
+module SCM
+  module Commits
+    class Git < Commit
+
+    end
+  end
+end
---
+++ lib/scm/commits/svn.rb
@@ -0,0 +1,9 @@
+require 'scm/commits/commit'
+
+module SCM
+  module Commits
+    class SVN < Commit
+
+    end
+  end
+end
